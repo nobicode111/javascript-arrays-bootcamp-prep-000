36adf501a69242d3941195b4b5ff4d85bd51f895
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-16 04:02:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,3 +19,7 @@ function accessElementInArray(array, index) {
   var element = array[index]
   return element
 }
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
