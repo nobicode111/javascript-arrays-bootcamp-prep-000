4bf2171c0bbe623a9f188266c2f7107ef6563721
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-16 04:19:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
-function removeelementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array) {
   array = array.slice(0, array.length - 1)
   return array
 }
