1f25e1365d9c8655869138e47a0b637bd6f3e7c0
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-16 03:48:53 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+function addElementToBeginningOfArray(array, n) {
+  var newArray = [n, ...array]
+  return newArray
+}
+function destructivelyAddElementToBeginningOfArray(array, n) {
+  array.unshift(n)
+  return array
+}
+function addElementToEndOfArray (array, n) {
+  var newArray = [...array, n]
+  return newArray
+}
+function destructivelyAddElementToEndOfArray (array, n) {
+  array.push(n)
+  return array
+}
